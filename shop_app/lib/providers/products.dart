4b02fc6ddd8e0dc3d75fa0cadf86ae2d39a850fb
amerfathullah <amerfathullah@rocketmail.com>
2022-02-01 23:28:47 +0800

    Auth: use ProxyProvider

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -43,6 +43,9 @@ class Products with ChangeNotifier {
   ];
 
   // var _showFavoritesOnly = false;
+  final String authToken;
+
+  Products(this.authToken, this._items);
 
   List<Product> get items {
     // if (_showFavoritesOnly) {
@@ -71,7 +74,7 @@ class Products with ChangeNotifier {
 
   Future<void> fetchAndSetProducts() async {
     final url = Uri.parse(
-        'https://shop-app-f4e11-default-rtdb.asia-southeast1.firebasedatabase.app/products.json');
+        'https://shop-app-f4e11-default-rtdb.asia-southeast1.firebasedatabase.app/products.json?auth=$authToken');
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
